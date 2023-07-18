32a2291febfe42a263563a92fb6e085d3c2bb516
Athiya Fathima <85676437+Athiya123@users.noreply.github.com>
2023-07-18 11:47:18 +0530

    modelschema

---
+++ server/models/Timesheet.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const timesheetSchema = new mongoose.Schema({
+  employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true },
+  weekStartDate: { type: Date, required: true },
+  hoursWorked: { type: Object, required: true },
+  documents: { type: [String] },
+  approved: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model('Timesheet', timesheetSchema);
