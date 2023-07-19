e666450ddd66e78169eb524fb7a5703382d8fd08
sowmya19454 <sowmya19454@gmail.com>
2023-07-19 16:55:53 +0530

    created login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import React, { useState } from "react";
+import Login from "./components/Login";
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 const App = () => {
-  return(
-    <div>
-      React App
-    </div>
-  )
-}
-
+  return (
+    <Router>
+      <ToastContainer />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<Login />}
+        />
+     </Routes>
+    </Router>
+  );
+};
 export default App;
