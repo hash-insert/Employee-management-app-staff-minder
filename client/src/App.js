918df98b79d9f160bef5be5b1c8807b6568fd16f
sowmya19454 <sowmya19454@gmail.com>
2023-07-19 17:22:50 +0530

    created dashboard ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
+import React from "react";
+import EmployeeDashboard from "./components/employee-components/Dashbaord";
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 const App = () => {
-  return(
-    <div>
-      React App
-    </div>
-  )
-}
 
+  return (
+    <Router>
+      <ToastContainer />
+      <Routes>
+        <Route
+          exact
+          path="/employee-dashboard"
+          element={
+            <EmployeeDashboard  />
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
 export default App;
