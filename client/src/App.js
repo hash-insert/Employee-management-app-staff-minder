fa7801f8964cfb4c02b9e8721d47521d05609c23
sowmya19454 <sowmya19454@gmail.com>
2023-07-19 17:07:03 +0530

    created reset password ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+import React  from "react";
+import Reset from "./components/Reset";
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 const App = () => {
-  return(
-    <div>
-      React App
-    </div>
-  )
-}
-
+  return (
+    <Router>
+      <ToastContainer />
+      <Routes>
+        <Route exact path="/reset" element={<Reset />} />
+      </Routes>
+    </Router>
+  );
+};
 export default App;
