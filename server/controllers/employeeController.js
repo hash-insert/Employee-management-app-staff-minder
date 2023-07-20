4afd9be03f887655a5ed6cda7ad2cbd59da86212
Athiya Fathima <85676437+Athiya123@users.noreply.github.com>
2023-07-20 15:13:20 +0530

    employeeController

---
+++ server/controllers/employeeController.js
@@ -0,0 +1,42 @@
+const Employee = require('../models/Employee');
+const Timesheet = require('../models/Timesheet');
+
+exports.getEmployeeProfile = async (req, res, next) => {
+    try {
+        const {employeeId} = req.params;
+        const employee = await Employee.findById(employeeId);
+        if (! employee) {
+            return res.status(404).json({message: 'Employee not found'});
+        }
+        res.json(employee);
+    } catch (error) {
+        next(error);
+    }
+};
+exports.submitTimesheet = async (req, res, next) => {
+    try {
+        const {employeeId, weekStartDate, hoursWorked, documents} = req.body;
+        const employee = await Employee.findById(employeeId);
+        if (! employee) {
+            return res.status(404).json({message: 'Employee not found'});
+        }
+        const timesheet = new Timesheet({employeeId, weekStartDate, hoursWorked, documents});
+        await timesheet.save();
+        res.status(201).json(timesheet);
+    } catch (error) {
+        next(error);
+    }
+};
+exports.getEmployeeTimesheets = async (req, res, next) => {
+    try {
+        const {employeeId} = req.params;
+        const employee = await Employee.findById(employeeId);
+        if (! employee) {
+            return res.status(404).json({message: 'Employee not found'});
+        }
+        const timesheets = await Timesheet.find({employeeId});
+        res.json(timesheets);
+    } catch (error) {
+        next(error);
+    }
+};
